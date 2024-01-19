7a2b265f0e9f9f86afdc7b529b901fe5acfcc174
João Pedro Silva <jp.pianissoli@gmail.com>
2024-01-19 11:27:49 +0000

    feat(c): added array exercices

---
+++ c/arrays/algorithms_in_c/3_11.c
@@ -0,0 +1,21 @@
+// Suppose that a is declared as int a[99]. Give the contents of the array
+// after the following two statements are executed: 
+// for (i = 0; i < 99; i++) a[i] = 98-i; 
+// for (i = 0; i < 99; i++) a[i] = a[a[i]]; 
+
+
+#include <stdio.h>
+
+int main(void) {
+    int a[99];
+
+    // 98, 97, 96, ... 0
+    for (int i = 0; i < 99; i++) a[i] = 98-i;
+
+    // 0, 1, 2 ... 49 -> 48, 47, ... 0
+    for (int i = 0; i < 99; i++) a[i] = a[a[i]];
+
+    for (int i = 0; i < 99; i++) printf("%d ", a[i]);
+
+    return 0;
+}
---
+++ c/arrays/algorithms_in_c/3_12.c
@@ -0,0 +1,45 @@
+// Modify our implementation of the sieve of Eratosthenes (Program 3.5)
+// to use an array of (i) chars; and (ii) bits. Determine the effects of these changes
+// on the amount of space and time used by the program. 
+
+
+#include <stdio.h>
+
+#define N 10000
+
+// size 17K
+// time 0.005s
+int sieve_original(void) {
+    int i, j, a[N];
+
+    for (i = 2; i < N; i++) a[i] = 1;
+
+    for(i = 2; i < N; i++)
+        if (a[i])
+            for (j = i; i*j < N; j++) a[i*j] = 0;
+
+    for (i = 2; i < N; i++)
+        if (a[i]) printf("%4d ", i);
+    printf("\n");
+
+    return 0;
+}
+
+// size 17K
+// time 0.004s
+int sieve_using_chars(void) {
+    int i, j;
+    char a[N];
+
+    for (i = 2; i < N; i++) a[i] = '1';
+
+    for (i = 2; i < N; i++)
+        if (a[i] == '1')
+            for (j = i; i*j < N; j++) a[i*j] = '0';
+
+    for (i = 2; i < N; i++)
+        if (a[i] == '1') printf("%4d ", i);
+    printf("\n");
+
+    return 0;
+}
