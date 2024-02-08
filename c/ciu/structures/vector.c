7543f03e0b4610b6d9336a1ac4ba93194ef6bca1
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2024-02-08 18:46:10 -0300

    chore(c): continued vector.c implementation

--- c/ciu/structures/vector.c
+++ c/ciu/structures/vector.c
@@ -2,19 +2,41 @@
 #include <stdlib.h>
 
 #define INITIAL_CAPACITY 16
-#define __array(T, N) typeof (typeof (T[N]))
-#define vector(T) struct { __array(T, INITIAL_CAPACITY) arr; int size; int capacity; }
-#define __initialize(T) malloc(sizeof(vector (T)))
+#define INITIAL_SIZE 0
 
-
-int size(size_t arr_size, size_t type_size) {
-	return arr_size/type_size;
-}
-
-
+#define __is_full(vec) (vec.__size == vec.__capacity - 1)
+// Add error log
+#define __resize(vec) \
+	vec.__addr = (typeof (vec.__addr)) realloc(vec.__addr, vec.__capacity * 2); \
+	vec.__capacity *= 2
+#define vector_t(T) \
+	struct { \
+		typeof (T *)__addr; \
+		int __size; \
+		int __capacity; \
+	}
+#define initialize_vector(T) \
+	{ \
+		(typeof (T *)) malloc(sizeof(vector_t (T)) * INITIAL_CAPACITY), \
+		INITIAL_SIZE, \
+		INITIAL_CAPACITY \
+	}
+#define size(vec) vec.__size
+#define push(item, vec) \
+	if(__is_full(vec)) \
+		__resize(vec); \
+	vec.addr[size(vec) + 1] = item; \
 
 int main(void) {
-	// Como inicializar isso?
-	vector (int) a =__initialize(int);
+	vector_t (int) a = initialize_vector(int);
+
+	for (int i = 0; i < INITIAL_CAPACITY; i++) 
+		a.arr[i] = i;
+
+	for (int i = 0; i < INITIAL_CAPACITY; i++) 
+		printf("%d ", a.arr[i]);
+
+	printf("\n%d\n", size(a));
+
 }
 
