e769751d5c0c7f1e4cce8cf6cde4c0f1c6b16607
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2024-06-03 17:49:40 -0300

    Fixed recursive_bs script

--- python/aalc7/recursive_binary_search.py
+++ python/aalc7/recursive_binary_search.py
@@ -13,10 +13,18 @@ def recursive_bs(arr: List[int], target: int) -> int:
 
     # offset is being summed even when element is not in array
     if target > mid:
-        return len(arr[:mid_pos]) + recursive_bs(arr[mid_pos:], target)
+        pos_found = recursive_bs(arr[mid_pos:], target)
+
+        if pos_found == -1:
+            return -1
+
+        return len(arr[:mid_pos]) + pos_found
     else:
         return recursive_bs(arr[:mid_pos], target)
 
 if __name__ == "__main__":
+    print(recursive_bs([1, 2, 3, 4, 5, 6], 1))
+    print(recursive_bs([1, 2, 3, 4, 5, 6], 6))
+    print(recursive_bs([1, 2, 3, 4, 5, 6], 4))
     print(recursive_bs([1, 2, 3, 4, 5, 6], 12))
 
